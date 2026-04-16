b02b47a3a38eac9baf67065f16aa57ec73c8cd08
0Heitor <heitortanizaka@terra.com.br>
2026-04-15 23:51:45 -0300

    commit 1

--- servicos/emailService.js
+++ servicos/emailService.js
@@ -18,6 +18,9 @@ export async function enviarEmailRecuperacao(emailUsuario) {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
